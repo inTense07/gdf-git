0e39e89a498eaa232b78e988cc97c52aaeee4ce1
test <test@test.com>
2023-10-09 17:50:16 -0300

    Primer commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+ return(
+  <div className='universo'>
+    <h1 className='letra'>Blog Universo 25.</h1>
+  
+ 
+ </div> 
+  
+  <div>
+    <form>
+  
+  <input    placeholder='nombre'>
+  </input>
+
+  </form>
+  </div>
+  
+  
+  
+  )
+
+  
+
+
+
+  
+
+}
+
+export default App;
